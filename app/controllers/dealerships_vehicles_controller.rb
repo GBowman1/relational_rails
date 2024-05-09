efcdcdf3164f44250305052e7f629bce9f731b06
Garrett Bowman <garrettbowman248@gmail.com>
2024-05-08 20:10:52 -0500

    Ready to move to next phase

--- app/controllers/dealerships_vehicles_controller.rb
+++ app/controllers/dealerships_vehicles_controller.rb
@@ -1,6 +1,8 @@
 class DealershipsVehiclesController < ApplicationController
     def index
         @vehicles = Dealership.find(params[:dealership_id]).vehicles
+        # nested resources research more
+        # finds all children belonging to a parent
     end
 
 end
