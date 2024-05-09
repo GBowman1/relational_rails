b4459ff9aa8cc483053077f3dbea6b262a777572
Garrett Bowman <garrettbowman248@gmail.com>
2024-05-09 12:45:20 -0500

    Set up spec for next iteration

--- app/controllers/dealerships_vehicles_controller.rb
+++ app/controllers/dealerships_vehicles_controller.rb
@@ -1,8 +1,6 @@
 class DealershipsVehiclesController < ApplicationController
     def index
         @vehicles = Dealership.find(params[:dealership_id]).vehicles
-        # nested resources research more
-        # finds all children belonging to a parent
     end
 
 end
