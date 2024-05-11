ce2d255d998fc22374e35a4540038d5e5b21a2c6
Garrett Bowman <garrettbowman248@gmail.com>
2024-05-11 14:20:02 -0500

    added test for dealership-show parent-child links

--- spec/features/dealerships/show_spec.rb
+++ spec/features/dealerships/show_spec.rb
@@ -42,5 +42,38 @@ describe "Dealerships Show" do
             # expect(page).to_not have_content("days open: #{dealer_2.days_open}")
             # expect(page).to_not have_content("rating: #{dealer_2.review_rating}")
         end
+        it 'has a link to the dealerships vehicles index' do
+            dealer_1 = Dealership.create!(
+                                        review_rating: 4,
+                                        days_open: "Monday - Friday",
+                                        city: "City 1",
+                                        name: "Dealer 1", 
+                                        military_discount: true,
+                                        created_at: Time.now,  
+                                        updated_at: Time.now
+            )
+            dealer_2 = Dealership.create!(
+                                        review_rating: 5,
+                                        days_open: "Monday - Saturday",
+                                        city: "City 2",
+                                        name: "Dealer 2",
+                                        military_discount: false,
+                                        created_at: Time.now,
+                                        updated_at: Time.now
+            )
+
+            visit "/dealerships/#{dealer_1.id}"
+
+            expect(page).to have_link("View #{dealer_1.name} Inventory")
+
+            click_link("View #{dealer_1.name} Inventory")
+            expect(current_path).to eq("/dealerships/#{dealer_1.id}/vehicles")
+
+            visit "/dealerships/#{dealer_2.id}"
+
+            expect(page).to have_link("View #{dealer_2.name} Inventory")
+            click_link("View #{dealer_2.name} Inventory")
+            expect(current_path).to eq("/dealerships/#{dealer_2.id}/vehicles")
+        end
     end
 end
